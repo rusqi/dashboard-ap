7a7986ca87c5bef317ed0e28b8cecea927cbbb1f
Muhammad Rusqi <muhammadrusqi10@gmail.com>
2024-10-05 15:16:28 +0700

    Update dashboard.py

--- submission/dashboard/dashboard.py
+++ submission/dashboard/dashboard.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 # Memuat dataset
-day_df = pd.read_csv("./dashboard/day.csv")
-hour_df = pd.read_csv("./dashboard/hour.csv")
+day_df = pd.read_csv("./submission/dashboard/dashboard/day.csv")
+hour_df = pd.read_csv("./submission/dashboard/dashboard/hour.csv")
 
 # Menggabungkan tabel day_df dan hour_df
 day_hour_df = pd.merge(
